9d86b1dda38d2a56abec1ad06b7e482111452a9f
Fifciuu <filip.jdrasik@gmail.com>
2022-09-04 17:58:31 +0200

    feat(vite): fade transition for routes

--- client-vite/src/App.vue
+++ client-vite/src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style>
-* { margin:0; padding:0; }
+* {
+  margin: 0;
+  padding: 0;
+}
+
 body {
   color: #fff;
   font-family: 'Ubuntu', sans-serif;
-  background: rgb(252,194,192);
-  background: linear-gradient(90deg, rgba(252,194,192,1) 0%, rgba(0,108,173,1) 100%);
+  background: rgb(252, 194, 192);
+  background: -moz-linear-gradient(90deg, rgba(252, 194, 192, 1) 0%, rgba(0, 108, 173, 1) 100%);
+  background: -webkit-linear-gradient(90deg, rgba(252, 194, 192, 1) 0%, rgba(0, 108, 173, 1) 100%);
+  background: linear-gradient(90deg, rgba(252, 194, 192, 1) 0%, rgba(0, 108, 173, 1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#fcc2c0", endColorstr="#006cad", GradientType=1);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
